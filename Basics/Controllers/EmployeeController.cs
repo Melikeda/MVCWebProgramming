61829007723cafdabea59e0a2dcd31e7bb9f8a3d
Melike Külahcı <m.edakulahci@gmail.com>
2026-03-06 19:43:11 +0300

    mesaj

---
+++ Basics/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using Basics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index1()
+        {
+            string message = $"Hello World. {DateTime.Now.ToString()}";
+            return View("Index1", message);
+        }
+
+        public ViewResult Index2()
+        {
+            var names = new String[]
+            {
+                "Ali",
+                "Veli",
+                "Can"
+            };
+            return View(names);
+        }
+
+        public IActionResult Index3() //Bu metodun döndürdüğü tür.Bu, metot farklı türde cevaplar (View, JSON, Redirect, Content vs.) döndürebileceği anlamına gelir.s
+        {
+            var list = new List<Employee>()
+            {
+                new Employee() { Id = 1, FirstName = "Melike", LastName = "Kulahci", Age = 24 },
+                new Employee() { Id = 2, FirstName = "Deniz", LastName = "Yol", Age = 35 },
+                new Employee() { Id = 3, FirstName = "Adem", LastName = "Gunes", Age = 12 }
+            };
+            return View("Index3", list);
+        }
+    }
+}
