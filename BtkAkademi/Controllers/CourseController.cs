403ec8fc23bb93073cd7cede85c49c3613169f74
Melike Külahcı <m.edakulahci@gmail.com>
2026-03-12 23:22:41 +0300

    commit

---
+++ BtkAkademi/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BtkAkademi.Models;
+
+namespace BtkAkademi.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]  //Bu attribute action metodunun sadece POST istekleri ile çağrılabileceğini söyler.
+        [ValidateAntiForgeyToken]  //Bu attribute Cross-Site Request Forgery (CSRF) saldırılarını önler.
+        public IActionResult Apply(Candidate model) //model binding
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ BtkAkademi/Models/Candidate.cs
@@ -0,0 +1,25 @@
+﻿namespace BtkAkademi.Models
+{
+    public class Candidate
+    {
+        public string? EMail { get; set; } = string.Empty;  //Buradaki ? işareti C#’ta nullable reference type anlamına gelir. Yani bu değişken null olabilir demektir.
+
+        public string? FirstName { get; set; } = string.Empty;
+
+        public string? LastName { get; set; } = string.Empty;
+
+        public string? FullName => $"{FirstName}{LastName?.ToUpper()}";
+
+        public int? Age { get; set; }
+
+        public string? SelectedCourse { get; set; } = string.Empty;
+
+        public DateTime ApplyAt { get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+
+    }
+}
